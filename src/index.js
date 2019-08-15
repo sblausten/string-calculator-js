400e050d9f93e8e0779ce64dd4ac45ce4fe077ef
Samuel Blausten <samuel.blausten@bbc.co.uk>
2019-08-15 15:07:02 +0100

    Added additional delimiters logic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,58 @@
 
-function add(numbersAsString) {
-  return numbersAsString
-    .split(/[,\n]/)
-    .map((numberAsString) => parseInt(numberAsString))
+function add(string) {
+  let delimitersRegex = /[,\n]/;
+  let numbersAsString = string;
+
+  if (string.startsWith("\\")) {
+    delimitersRegex = _updateDelimitersRegex(string, delimitersRegex);
+    numbersAsString = string.slice(string.indexOf('\n') + 1);
+  }
+
+  const numbersAsStringsArray = numbersAsString
+    .split(delimitersRegex)
+    .filter((element) => element.length > 0);
+
+  const numbersAsArray = numbersAsStringsArray
+    .map((numberAsString) => parseInt(numberAsString));
+
+  const sum = numbersAsArray
     .reduce((a, b) => a+b);
+
+  return sum;
 }
 
-module.exports = add;
+function _extractDelimiters(stringWithDelimiters) {
+  let delimitersString = stringWithDelimiters
+  .substring(2, stringWithDelimiters.indexOf('\n'));
+
+  const extractedDelimiters = [];
+  while (delimitersString.includes('[')) {
+    const { remainingDelimiters, extractedDelimiter } = _getDelimiterAndRemainingString(delimitersString);
+    extractedDelimiters.push(extractedDelimiter);
+    delimitersString = remainingDelimiters;
+  }
+
+  return extractedDelimiters;
+}
+
+function _updateDelimitersRegex(string, delimitersRegex) {
+  const additionalDelimitersArray = _extractDelimiters(string)
+  const additionalDelimiters = additionalDelimitersArray
+  .map((delimiter) => '(' + delimiter + ')')
+  .join();
+  delimitersRegex = new RegExp('[,\\n' + additionalDelimiters + ']');
+  return delimitersRegex;
+};
+
+function _getDelimiterAndRemainingString(stringWithDelimiters) {
+  let start = stringWithDelimiters.indexOf('[');
+  let end = stringWithDelimiters.indexOf(']');
+  const extractedDelimiter = stringWithDelimiters.substring(start + 1, end);
+
+  let remainingDelimiters = stringWithDelimiters.slice(end + 1, stringWithDelimiters.length);
+  return { remainingDelimiters, extractedDelimiter };
+}
+
+
+
+module.exports.add = add;
--- test/index.test.js
+++ test/index.test.js
@@ -1,37 +1,54 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
 const sandbox = sinon.createSandbox();
-const add = require('../src/index');
+const add = require('../src/index').add;
 
 describe('add', () => {
 
-  it('should return single number', () => {
-    const result = add('2');
+  describe('should return sum', () => {
+    it('of single number', () => {
+      const result = add('2');
+      expect(result).to.equal(2);
+    });
 
-    expect(result).to.equal(2);
-  });
+    it('of two comma separated numbers', () => {
+      const result = add('1,1');
+      expect(result).to.equal(2);
+    });
 
-  it('should return sum of two comma separated numbers', () => {
-    const result = add('1,1');
+    it('of two other comma separated numbers', () => {
+      const result = add('2,2');
+      expect(result).to.equal(4);
+    });
 
-    expect(result).to.equal(2);
-  });
+    it('of more than 2 comma separated numbers', () => {
+      const result = add('1,1,1');
+      expect(result).to.equal(3);
+    });
 
-  it('should return sum of two other comma separated numbers', () => {
-    const result = add('2,2');
+    it('of two new space separated numbers', () => {
+      const result = add('1\n1');
+      expect(result).to.equal(2);
+    });
 
-    expect(result).to.equal(4);
-  });
+    it('of two new space separated numbers', () => {
+      const result = add('1\n1');
+      expect(result).to.equal(2);
+    });
 
-  it('should return sum of more than 2 comma separated numbers', () => {
-    const result = add('1,1,1');
+    it('of two numbers with different delimiter', () => {
+      const result = add("\\\\[**]\n1**1");
+      expect(result).to.equal(2);
+    });
 
-    expect(result).to.equal(3);
-  });
+    it('of three numbers with different delimiters', () => {
+      const result = add("\\\\[**]\n1**1,1");
+      expect(result).to.equal(3);
+    });
 
-  it('should return sum of two new space separated numbers', () => {
-    const result = add('1\n1');
-
-    expect(result).to.equal(2);
+    it('of four numbers with several different delimiters', () => {
+      const result = add("\\\\[**][|]\n1**1,1|1");
+      expect(result).to.equal(4);
+    });
   });
 });
